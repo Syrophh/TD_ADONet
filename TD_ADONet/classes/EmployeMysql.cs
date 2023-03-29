0b5c86ae5810d61c1b7dc3e75f29ab97e1bb7ce4
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-03-29 11:52:19 +0200

    oracle partie 2

---
+++ TD_ADONet/classes/EmployeMysql.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeDatas.Oracle
+{
+    class EmployeMysql
+    {
+        private String host;
+        private int port;
+        private String db;
+        private String login;
+        private String pwd;
+        private MySqlConnection connexion;
+
+        public EmployeMysql(string host, int port, string db, string login, string pwd)
+        {
+            this.host = host;
+            this.port = port;
+            this.db = db;
+            this.login = login;
+            this.pwd = pwd;
+        }
+    }
+}
---
+++ TD_ADONet/classes/EmployeOracle.cs
@@ -0,0 +1,29 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeDatas.Oracle
+{
+    class EmployeOracle
+    {
+        private String host;
+        private int port;
+        private String db;
+        private String login;
+        private String pwd;
+        private OracleConnection connexion;
+
+        public EmployeOracle(string host, int port, string db, string login, string pwd)
+        {
+            this.host = host;
+            this.port = port;
+            this.db = db;
+            this.login = login;
+            this.pwd = pwd;
+            this.connexion = new OracleConnection($"SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT={port}))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));uid={login};pwd={pwd};");
+        }
+    }
+}
