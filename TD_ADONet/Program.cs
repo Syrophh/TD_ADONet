56712b006676da5520ada5c540be3c43d70ffa52
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-04-03 16:33:00 +0200

    méthode oracle afficheSalaireEmploye

--- TD_ADONet/Program.cs
+++ TD_ADONet/Program.cs
@@ -21,31 +21,39 @@ namespace TD_ADONet
             try
             {
                 EmployeOracle empOracle = new EmployeOracle(host, port, sid, login, pwd);
+                Console.WriteLine("Afficher tous les cours");
+                empOracle.afficherTousLesCours();
+                Console.WriteLine("Afficher le nombre de projet");
+                empOracle.afficherNbProjets();
+                Console.WriteLine("Afficher le salaire moyen par projet");
+                empOracle.afficherSalaireMoyenParProjet();
+                Console.WriteLine("Augmenter le salaire des employés qui travaillent sur le projet PR1");
+                empOracle.augmenterSalaireCurseur();
             }
             catch(OracleException ex)
             {
                 Console.WriteLine("Erreur Oracle " + ex.Message);
             }
 
-            string hostMysql = "127.0.0.1";
-            int portMysql = 3306;
-            string baseMysql = "dbadonet";
-            String uidMysql = "employeado";
-            String pwdMysql = "employeado";
-            try
-            {
-                String csMysql = String.Format("Server = {0}; Port = {1}; Database = {2}; " +
-                    "Uid = {3};"+ "Pwd = {4}", hostMysql, portMysql, baseMysql, uidMysql, pwdMysql);
-                MySqlConnection cnMysql = new MySqlConnection(csMysql);
-                cnMysql.Open();
-                Console.WriteLine("connecté Mysql");
-                cnMysql.Close();
-                Console.WriteLine("déconnecté Mysql");
-            }
-            catch(MySqlException ex)
-            {
-                Console.WriteLine("Erreur Mysql " + ex.Message);
-            }
+            //string hostMysql = "127.0.0.1";
+            //int portMysql = 3306;
+            //string baseMysql = "dbadonet";
+            //String uidMysql = "employeado";
+            //String pwdMysql = "employeado";
+            //try
+            //{
+            //    String csMysql = String.Format("Server = {0}; Port = {1}; Database = {2}; " +
+            //        "Uid = {3};"+ "Pwd = {4}", hostMysql, portMysql, baseMysql, uidMysql, pwdMysql);
+            //    MySqlConnection cnMysql = new MySqlConnection(csMysql);
+            //    cnMysql.Open();
+            //    Console.WriteLine("connecté Mysql");
+            //    cnMysql.Close();
+            //    Console.WriteLine("déconnecté Mysql");
+            //}
+            //catch(MySqlException ex)
+            //{
+            //    Console.WriteLine("Erreur Mysql " + ex.Message);
+            //}
             Console.ReadKey();
         }
     }
--- TD_ADONet/classes/EmployeOracle.cs
+++ TD_ADONet/classes/EmployeOracle.cs
@@ -23,7 +23,7 @@ namespace EmployeDatas.Oracle
             this.db = db;
             this.login = login;
             this.pwd = pwd;
-            this.connexion = new OracleConnection($"SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT={port}))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));uid={login};pwd={pwd};");
+            this.connexion = new OracleConnection($"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT={port}))(CONNECT_DATA=(SERVICE_NAME={db})));User Id={login};Password={pwd};");
         }
 
         public void Ouvrir()
@@ -49,11 +49,128 @@ namespace EmployeDatas.Oracle
                     String numCours = unReader.GetString(0);
                     String libelleCours = unReader.GetString(1);
                     int nbJours = unReader.GetInt32(2);
-                    Console.WriteLine("Numéro du cours = : {0}\n Libellé du cours : {1}\n Nombre de jours : {2}", numCours);
+                    Console.WriteLine("Numéro du cours = : {0}\n Libellé du cours : {1}\n Nombre de jours : {2}", numCours, libelleCours, nbJours);
                 }
                 Fermer();
             }
             catch(OracleException ex)
+            {
+                Console.WriteLine("Erreur Oracle " + ex.Message);
+            }
+        }
+
+        public void afficherNbProjets()
+        {
+            string requete = "select count(*) as nbProjet from projet";
+            try
+            {
+                Ouvrir();
+                OracleCommand commande = new OracleCommand(requete, this.connexion);
+                var unReader = commande.ExecuteReader();
+                while (unReader.Read())
+                {
+                    int nbProjet = unReader.GetInt32(0);
+                    Console.WriteLine("Le nombre de projet : {0}\n", nbProjet);
+                }
+                Fermer();
+            }
+            catch(OracleException ex)
+            {
+                Console.WriteLine("Erreur Oracle " + ex.Message);
+            }
+        }
+
+        public void afficherSalaireMoyenParProjet()
+        {
+            string requete = "select coalesce(projet.codeprojet, 'aucun') as codeprojet, projet.nomprojet, avg(employe.salaire) as moyenne " +
+                "from projet inner join employe on employe.codeprojet = projet.codeprojet " +
+                "group by projet.codeprojet, projet.nomprojet";
+            try
+            {
+                Ouvrir();
+                OracleCommand commande = new OracleCommand(requete, this.connexion);
+                var unReader = commande.ExecuteReader();
+                while (unReader.Read())
+                {
+                    String codeProjet = unReader.GetString(0);
+                    String nomProjet = unReader.GetString(1);
+                    int moyenne = unReader.GetInt32(2);
+                    Console.WriteLine("Code du projet : {0}\n Nom du projet : {1}\n Moyenne des salaires par projet : {2}\n", codeProjet, nomProjet, moyenne);
+                }
+                Fermer();
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine("Erreur Oracle " + ex.Message);
+            }
+        }
+
+        public void augmenterSalaireCurseur()
+        {
+            string requete = "select * from employe where codeprojet = 'PR1'; ";
+            try
+            {
+                Ouvrir();
+                OracleCommand command = new OracleCommand(requete, this.connexion);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                var unReader = command.ExecuteReader();
+                while (unReader.Read())
+                {
+                    requete = "update employe set salaire = salaire * 1.03";
+                    command.CommandText = requete;
+                    Console.WriteLine("Employes modifiés");
+                }
+                Fermer();
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine("Erreur Oracle " + ex.Message);
+            }
+        }
+
+        public void afficherEmployesSalaire()
+        {
+            string requete = "select numemp, nomemp, prenomemp, salaire from employe where salaire< 10000";
+             try
+             {
+                 Ouvrir();
+                 OracleCommand commande = new OracleCommand(requete, this.connexion);
+                 var unReader = commande.ExecuteReader();
+                 while (unReader.Read())
+                 {
+                    String numEmp = unReader.GetString(0);
+                    String nomEmp = unReader.GetString(1);
+                    String prenomEmp = unReader.GetString(2);
+                    int salaire = unReader.GetInt32(3);
+                    Console.WriteLine("Numero de l'employe : {0}\n Nom de l'employe : {1}\n Prenom de l'employe : {2}\n salaire de l'employe : {3}", numEmp, nomEmp, prenomEmp, salaire);
+                 }
+                 Fermer();
+             }
+             catch (OracleException ex)
+             {
+                 Console.WriteLine("Erreur Oracle " + ex.Message);
+             }
+        }
+
+        public void afficherSalaireEmploye()
+        {
+            string requete = "select numemp, nomemp, prenomemp, salaire from employe where numemp = 76";
+            try
+            {
+                Ouvrir();
+                OracleCommand commande = new OracleCommand(requete, this.connexion);
+                var unReader = commande.ExecuteReader();
+                while (unReader.Read())
+                {
+                    String numEmp = unReader.GetString(0);
+                    String nomEmp = unReader.GetString(1);
+                    String prenomEmp = unReader.GetString(2);
+                    int salaire = unReader.GetInt32(3);
+                    Console.WriteLine("Numero de l'employe : {0}\n Nom de l'employe : {1}\n Prenom de l'employe : {2}\n salaire de l'employe : {3}", numEmp, nomEmp, prenomEmp, salaire);
+                }
+                Fermer();
+            }
+            catch (OracleException ex)
             {
                 Console.WriteLine("Erreur Oracle " + ex.Message);
             }
